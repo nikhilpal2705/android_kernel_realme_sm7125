6de8e567ff65c6aa112af08a82755ec3a2124025
nikhilpal2705 <nikhilpal2705@gmail.com>
2026-09-11 14:34:04 +0530

    touchpanel: report touch events on FingerprintDown and FingerprintUp for AOD UDFPS
    When the panel enters AOD (DOZE), touchpanel_enter_aod() configures
    the touch controller in gesture mode. In this mode, touches on the
    FOD sensor area trigger FingerprintDown and FingerprintUp gesture events.
    
    Previously, tp_gesture_handle() only notified opticalfp_irq_handler and
    notify_display_fpd, but did not dispatch any MT touch input events to
    ts->input_dev. Because Android's InputDispatcher received no motion events,
    UdfpsController in SystemUI was never notified of the touch, failing
    to invoke drawIlluminationDot(). This resulted in the missing pressed icon
    (udfps_icon_pressed.png) and missing HBM illumination behind the FOD icon on AOD.
    
    Report multi-touch input events (input_mt_slot, MT_TOOL_FINGER, BTN_TOUCH,
    and ABS_MT_POSITION_X/Y) on FingerprintDown and FingerprintUp. If gesture
    coordinates are zero, fall back to default screen center and FOD sensor Y offset.

--- drivers/input/touchscreen/oppo_touchscreen/touchpanel_common_driver.c
+++ drivers/input/touchscreen/oppo_touchscreen/touchpanel_common_driver.c
@@ -549,13 +549,28 @@ static void tp_gesture_handle(struct touchpanel_data *ts)
             input_sync(ts->input_dev);
         }
     } else if (gesture_info_temp.gesture_type == FingerprintDown) {
+        int x = gesture_info_temp.Point_start.x;
+        int y = gesture_info_temp.Point_start.y;
         ts->fp_info.touch_state = 1;
         if (ts->screenoff_fingerprint_info_support) {
-            ts->fp_info.x = gesture_info_temp.Point_start.x;
-            ts->fp_info.y = gesture_info_temp.Point_start.y;
+            ts->fp_info.x = x;
+            ts->fp_info.y = y;
         }
         opticalfp_irq_handler(&ts->fp_info);
         notify_display_fpd(true);
+        if (ts->input_dev) {
+            if (!x && !y && ts->resolution_info.max_x && ts->resolution_info.max_y) {
+                x = ts->resolution_info.max_x / 2;
+                y = ts->resolution_info.max_y - 203;
+            }
+            input_mt_slot(ts->input_dev, 0);
+            input_mt_report_slot_state(ts->input_dev, MT_TOOL_FINGER, 1);
+            input_report_key(ts->input_dev, BTN_TOUCH, 1);
+            input_report_key(ts->input_dev, BTN_TOOL_FINGER, 1);
+            input_report_abs(ts->input_dev, ABS_MT_POSITION_X, x);
+            input_report_abs(ts->input_dev, ABS_MT_POSITION_Y, y);
+            input_sync(ts->input_dev);
+        }
     } else if (gesture_info_temp.gesture_type == FingerprintUp) {
         ts->fp_info.touch_state = 0;
         if (ts->screenoff_fingerprint_info_support) {
@@ -564,6 +579,13 @@ static void tp_gesture_handle(struct touchpanel_data *ts)
         }
         opticalfp_irq_handler(&ts->fp_info);
         notify_display_fpd(false);
+        if (ts->input_dev) {
+            input_mt_slot(ts->input_dev, 0);
+            input_mt_report_slot_state(ts->input_dev, MT_TOOL_FINGER, 0);
+            input_report_key(ts->input_dev, BTN_TOUCH, 0);
+            input_report_key(ts->input_dev, BTN_TOOL_FINGER, 0);
+            input_sync(ts->input_dev);
+        }
     }
 }
 
